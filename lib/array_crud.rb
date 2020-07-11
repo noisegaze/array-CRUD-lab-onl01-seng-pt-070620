bfd8f57f0e39319ec0d49cdc0a7a97399d16d3f4
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-11 19:09:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
   []
 end
-["apple","orange","plum"]
-def create_an_array
 
+def create_an_array
+["apple","orange","plum"]
 end
 
 def add_element_to_end_of_array(array, element)
